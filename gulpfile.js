958f168d11098681c536cde53368249e5403c820
wangzhizhi <2216453885@qq.com>
2022-04-09 19:28:16 +0800

    111

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require("gulp");
-var ts = require("gulp-typescript");
-// const rollup = require('gulp-rollup');
-var tsProject = ts.createProject("tsconfig.json");
-gulp.task("default", function () {
-  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
-});
