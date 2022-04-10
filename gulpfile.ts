5756480523e867fbef7bbe652ef2c2579b09a34e
wangzhizhi <2216453885@qq.com>
2022-04-10 10:43:52 +0800

    bug fixed

---
+++ gulpfile.ts
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+var ts = require('gulp-typescript');
+const rollup = require('gulp-rollup');
+const replace = require('@rollup/plugin-replace');
+//清洗环境
+function buildconfig() {
+    return gulp
+        .src('./dist/**/*.js')
+        .pipe(
+            rollup({
+                input: './dist/app.js',
+                output: {
+                    format: 'cjs',
+                },
+                plugins: [
+                    replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+                ],
+            })
+        )
+        .pipe(gulp.dest('./dist'));
+}
+function buildprod() {
+    return gulp.src('./src/**/*.ts')
+        .pipe(
+            babel({
+                babelrc: false,
+                plugins: ['@babel/plugin-transform-modules-commonjs', '@babel/plugin-transform-typescript', ["@babel/plugin-proposal-decorators", { "legacy": true }],],
+            })
+        )
+        .pipe(gulp.dest("dist"));
+}
+let build = gulp.series(buildprod, buildconfig);
+
+gulp.task("default", build);
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,27 @@
 
-import express = require('express');
-import bodyParser = require('body-parser')
+import express from 'express';
+import bodyParser from 'body-parser'
 import { asClass, createContainer } from 'awilix'
 import { loadControllers, scopePerRequest } from 'awilix-express'
-import { UserService } from './Services/userService'
+import { UserService } from './Services/UserService'
+
 
 const app = express()
 const container = createContainer()
     .register({
-        userService: asClass(UserService) 
+        userService: asClass(UserService)
     })
 
 
 app.use(bodyParser.json())
 app.use(scopePerRequest(container))
-
-app.use(loadControllers('Controller/*.ts', { cwd: __dirname }))
-
-app.listen(6000)
+let controllerUrl;
+if (process.env.NODE_ENV == 'production') {
+    controllerUrl = 'Controller/*.js'
+} else {
+    controllerUrl = 'Controller/*.ts'
+}
+app.use(loadControllers(controllerUrl, { cwd: __dirname }))
+app.listen(3000, () => {
+    console.log("启动成功：3000");
+})
